277deca90ea7e090792f5d854672cd8750f81d6b
2k4 <tacotakispam@gmail.com>
2025-06-21 19:19:15 -0400

    Changed Music Order

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ let isLoading = false;
 // Updated playlist configuration
 const playlists = {
   YourFavs: [
-    "Jugador 9.mp3",
-    "La Pantera.mp3",
     "Latinas Everywhere (Weke Weke).mp3",
     "Not Even Ghosts Are This Empty.mp3",
     "The Number You Have Dialed Is Not in Service.mp3",
-    "Club Bizarre.mp3"
+    "Club Bizarre.mp3",
+    "Jugador 9.mp3",
+    "La Pantera.mp3"
   ],
   ForYou: [
-    "Desire.mp3",
+    "Smile.mp3",
     "Flaws And Sins.mp3",
     "She’s The One.mp3",
+    "Desire.mp3",
     "Adore You.mp3",
     "999 (Alkaline).mp3",
-    "Smile.mp3",
     "I Want It.mp3"
   ]
 };
